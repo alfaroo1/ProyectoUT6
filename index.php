4a2d46afc65c1d9e3e771d637b6ca9ef9bca8bae
Carlos Alfaro Paniagua <alfaropaniagua313@gmail.com>
2024-12-20 20:11:45 +0100

    Add tailwindcss from style to the pages

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include "./views/ListarLibrosView.php";
-include "./controllers/LibrosController.php";
-include "./models/BibliotecaModels.php";
-
-$librosController = new LibrosController();
-
-$librosController->listar();
---
+++ src/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./output.css">
+    <title>Biblioteca | Inicio</title>
+</head>
+
+<body>
+    <!-- Container -->
+    <div>
+        <!-- Navegador -->
+        <div>
+            <div>
+                <!-- Logo Web -->
+                <img src="" alt="">
+                <!-- Nav -->
+                <nav>
+                    <ul>
+                        <li><a href="./index.php">Inicio</a></li>
+                        <li><a href="./pages/libros.php">Libros</a></li>
+                        <li><a href="#">Reservas</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <!-- Buttons users -->
+            <div>
+                <!-- Login -->
+                <button>Log in</button>
+                <!-- Register -->
+                <button>Register</button>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+<?php
